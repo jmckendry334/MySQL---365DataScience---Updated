1140952ccdfc00447b15a6a963c60453ebbdae40
jmckendry334 <52835344+jmckendry334@users.noreply.github.com>
2020-04-03 17:10:40 -0400

    Update SQL_Stored_Procedures.sql

--- SQL_Stored_Procedures.sql
+++ SQL_Stored_Procedures.sql
@@ -17,4 +17,27 @@ from salaries;
 end $$
 call AVG_SAL()
 
+##Create Procedure with an Input Paramater
+drop procedure if exists A_SALARY;
+delimiter $$
+create procedure A_SALARY(IN p_emp_no INT)
+begin
+select 
+e.first_name,
+e.last_name,
+AVG(s.salary),
+s.from_date,
+s.to_date
+from
+employees e 
+join salaries s on e.emp_no = s.emp_no
+where e.emp_no = p_emp_no;
+end $$
+delimiter ; 
+
+
+
+
+
+
 
